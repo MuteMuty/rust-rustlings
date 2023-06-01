7ac772dc1e74277b0d16dc4a90aedb6f2f9fd503
MuteMuty <69723817+MuteMuty@users.noreply.github.com>
2023-06-01 10:59:52 +0200

    initial commit

---
+++ clippy/target/debug/deps/clippy3-d794151b7ccd3da3.d
@@ -0,0 +1,8 @@
+C:\Users\Erik\Desktop\rust\rustlings\exercises\clippy\target\debug\deps\clippy3-d794151b7ccd3da3.rmeta: clippy3.rs Cargo.toml
+
+C:\Users\Erik\Desktop\rust\rustlings\exercises\clippy\target\debug\deps\clippy3-d794151b7ccd3da3.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
